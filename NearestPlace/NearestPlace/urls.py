4ebee5ee75df660b6a8b26adaf002650caa7dc0b
Ahmad <ahmad.hussain.307@gmail.com>
2018-07-21 03:06:22 +0200

    Added saving user location to a choosen meeting

--- NearestPlace/NearestPlace/urls.py
+++ NearestPlace/NearestPlace/urls.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from locations import views
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='home'),
     url(r'^locations/', include('locations.urls'), name='locations'),
     url(r'^get_location', views.get_location),
     url(r'^create_session', views.create_session),
-    url(r'^admin/', admin.site.urls),
+    url(r'^add_location', views.add_location)
 ]
--- NearestPlace/locations/views.py
+++ NearestPlace/locations/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, HttpResponse
 from locations.gmaps import LocationFinder
-from locations.models import Session
 from django.core.exceptions import ValidationError
+from locations.models import Session,Location
 import json
 import secrets
 import string
+from decimal import *
 
 def index(request):
     api_key = open('D:/Projects/secretkey/gmaps_key.txt','r').read()
@@ -36,6 +37,8 @@ def create_session(request):
             place_type = request.POST.get('place_type'),
             city = request.POST.get('city')
         )
+
+        # Validate data before saving to Database
         try:
             session.full_clean()
             session.save()
@@ -44,3 +47,31 @@ def create_session(request):
             return HttpResponse(json.dumps({'message':'ValidationError'}), content_type='application/json')
 
         return HttpResponse(json.dumps({'message':'success','code':code}), content_type='application/json')
+
+def add_location(request):
+    """Adds a new location to the given meeting and saves it in DB."""
+    if request.POST:
+        # Get request data
+        code = request.POST.get('code')
+        lat = round(Decimal(request.POST.get('lat')),6)
+        lng = round(Decimal(request.POST.get('lng')),6)
+
+        # Create object of location with the given Session's code
+        session = Session.objects.get(code=code)
+        location = Location(
+            longitude=lng,
+            latitude=lat,
+            session=session
+        )
+
+        # Validate data before saving to Database
+        try:
+            location.full_clean()
+            location.save()
+            print('location saved!')
+            result = {'message':'Success'}
+        except ValidationError as e:
+            print(e)
+            result = {'message':'Validaiton Error'}
+
+        return HttpResponse(json.dumps(result), content_type='application/json')
